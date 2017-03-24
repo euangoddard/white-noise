0d69b6923ae2ca3da92cd5d89807f17a85f5c0d9
Euan Goddard <euan.goddard@gmail.com>
2017-03-24 19:33:35 +0000

    Converted noise from white to pink noise

--- src/noise.ts
+++ src/noise.ts
@@ -7,16 +7,19 @@ export class Noise {
 
   private source = audioCtx.createBufferSource();
 
+  private pinkData = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
   constructor() {
     const buffer = audioCtx.createBuffer(
       Noise.CHANNELS,
       Noise.FRAME_COUNT, 
       audioCtx.sampleRate,
     );
+
     for (let channel = 0; channel < Noise.CHANNELS; channel++) {
       var nowBuffering = buffer.getChannelData(channel);
       for (var i = 0; i < Noise.FRAME_COUNT; i++) {
-        nowBuffering[i] = Math.random() * 2 - 1;
+        nowBuffering[i] = this.makePinkNoise();
       }
     }
     this.source.buffer = buffer;
@@ -31,4 +34,20 @@ export class Noise {
   stop(): void {
     this.source.disconnect(audioCtx.destination);
   }
+
+  private makePinkNoise(): number {
+    const white = Math.random() * 2 - 1;
+    this.pinkData[0] = 0.99886 * this.pinkData[0] + white * 0.0555179;
+    this.pinkData[1] = 0.99332 * this.pinkData[1] + white * 0.0750759;
+    this.pinkData[2] = 0.96900 * this.pinkData[2] + white * 0.1538520;
+    this.pinkData[3] = 0.86650 * this.pinkData[3] + white * 0.3104856;
+    this.pinkData[4] = 0.55000 * this.pinkData[4] + white * 0.5329522;
+    this.pinkData[5] = -0.7616 * this.pinkData[5] - white * 0.0168980;
+    const output =  (this.pinkData.reduce(sum, 0) + white * 0.5362) * 0.11;
+    this.pinkData[6] = white * 0.115926;
+    return output;
+  }
 }
+
+
+const sum = (a, b) => a + b;
